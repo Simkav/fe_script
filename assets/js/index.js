6795bfbd083c3cfc0630ed58e4c55fcc9f1025de
Ivan Khomenko <Simkav2411@gmail.com>
2021-01-13 18:12:49 +0200

    Class example

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,25 @@
 const MIN_ZP = 6000 / 21;
 
 class Worker {
-  constructor(name, surName, salaryRate = MIN_ZP, dayOnWork = 0) {
+  constructor(name, surName, salaryRate = 600, dayOnWork = 0) {
     this.name = name;
     this.surName = surName;
     this.salaryRate = salaryRate;
     this.dayOnWork = dayOnWork;
+  }
+
+  set salaryRate(newRate) {
+    if (typeof newRate !== 'number') {
+      throw new TypeError('Not a number');
+    }
+    if (newRate < 0) {
+      throw new RangeError();
+    }
+    this._salaryRate = newRate;
+  }
+
+  getSalary() {
+    return this.salaryRate * this.dayOnWork;
   }
 
   getFullName() {
@@ -22,13 +36,30 @@ class Worker {
     return ++this.dayOnWork;
   }
 
-  getSalary() {
-    return this.salaryRate * this.dayOnWork;
-  }
-
   issueSalary() {
     console.log(`Payed ${this.getSalary()} to ${this.getFullName()}`);
     this.workedDay = 0;
   }
 }
 const w1 = new Worker('John', 'Doe', 500, 5);
+
+class Friend {
+  constructor(name, appleAmount, friends) {
+    this.name = name;
+    this.appleAmount = appleAmount;
+    this.friends = friends;
+  }
+
+  getAllApple() {
+    let myApples = this.appleAmount;
+    this.friends.forEach((friend) => {
+      myApples += friend.appleAmount;
+    });
+    return myApples;
+  }
+}
+
+const me = new Friend('Ivan', 5, [
+  new Friend('John', 10),
+  new Friend('Keks', 20),
+]);
