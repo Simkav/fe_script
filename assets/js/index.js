c8b3e4161fc228f1a3e3e41b1ee8a0a69e6a3097
Simkav <Simkav2411@gmail.com>
2021-01-15 19:13:04 +0200

    Add User class

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,14 @@
 "use strict";
+
+class User {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+}
 {
   class MyArray {
     constructor() {
