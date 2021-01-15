d51d4701ff7f98fa0a9976ba056f4d24ea99d0b5
Simkav <Simkav2411@gmail.com>
2021-01-15 20:47:51 +0200

    TODO

--- assets/js/queue.js
+++ assets/js/queue.js
@@ -2,11 +2,23 @@
 class Queue {
   constructor() {
     this.size = 0;
+    this.firstIndex = 0;
   }
   enqueue(value) {
     this[this.size] = value;
     return this.size++;
   }
+  test() {
+    if (this.size === 0) {
+      return undefined;
+    }
+    const returnValue = this[this.firstIndex];
+    delete this[this.firstIndex];
+    // this.firstIndex = this.firstIndex + 1;
+    --this.size;
+    return returnValue;
+  }
+
   dequeue() {
     if (this.size === 0) {
       return undefined;
@@ -20,10 +32,16 @@ class Queue {
     return returnValue;
   }
   front() {
-    return this[0];
+    return this[this.firstIndex];
   }
   get isEmpty() {
-    return this._size === 0 ? true : false;
+    return !(this._size === 0);
+  }
+  set firstIndex(v) {
+    this._firstIndex = v;
+  }
+  get firstIndex() {
+    return this._firstIndex;
   }
   set size(v) {
     this._size = v;
@@ -31,5 +49,9 @@ class Queue {
   get size() {
     return this._size;
   }
+  [Symbol.iterator]() {
+    return new QueueIterator();
+  }
 }
+class QueueIterator {}
 const queue = new Queue();
