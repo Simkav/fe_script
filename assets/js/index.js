a3d8f3074e11762812829ef382f9824be6547981
Ivan Khomenko <Simkav2411@gmail.com>
2021-01-20 19:35:17 +0200

    Refactor compare

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,22 +3,20 @@ const compare = (firstString, secondString) => {
   if (firstString.length !== secondString.length) {
     throw new Error('Impossible');
   }
-  const firstMap = new Map();
-  const secondMap = new Map();
-  for (const char of firstString) {
-    firstMap.has(char)
-      ? firstMap.set(char, firstMap.get(char) + 1)
-      : firstMap.set(char, 1);
-  }
-  for (const char of secondString) {
-    secondMap.has(char)
-      ? secondMap.set(char, secondMap.get(char) + 1)
-      : secondMap.set(char, 1);
-  }
+  const firstMap = createVocabulary(secondString);
+  const secondMap = createVocabulary(secondString);
   for (const key of firstMap.keys()) {
     if (firstMap.get(key) !== secondMap.get(key)) {
       throw new Error('Impossible');
     }
   }
   return true;
+};
+
+const createVocabulary = (str) => {
+  const map = new Map();
+  for (const char of str) {
+    map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
+  }
+  return map;
 };
